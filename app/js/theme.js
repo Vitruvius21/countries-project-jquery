33fb6ce4e7862bab92e089a2070b5868565d874c
Vitruvius21 <balakhadze.dev@gmail.com>
2022-03-27 00:04:58 +0400

    fix(theme): add chaining

--- app/js/theme.js
+++ app/js/theme.js
@@ -1,4 +1,5 @@
 export function defineTheme() {
+  const body = $("body");
   const btnIcon = $(".btn-toggle");
 
   const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
@@ -6,10 +7,10 @@ export function defineTheme() {
 
   if (currentTheme == "dark") {
     btnIcon.addClass("btn-toggle-dark");
-    $("body").toggleClass("dark-theme");
+    body.toggleClass("dark-theme");
   } else if (currentTheme == "light") {
     btnIcon.addClass("btn-toggle-light");
-    $("body").toggleClass("light-theme");
+    body.toggleClass("light-theme");
   }
 
   if (prefersDarkScheme && prefersDarkScheme?.matches) {
@@ -19,15 +20,13 @@ export function defineTheme() {
   $(".theme__btn-container").on("click", function () {
     let theme;
 
-    btnIcon.addClass("btn-toggle-dark");
-    btnIcon.toggleClass("btn-toggle-light");
+    btnIcon.addClass("btn-toggle-dark").toggleClass("btn-toggle-light");
 
     if (prefersDarkScheme && prefersDarkScheme?.matches) {
-      $("body").removeClass("dark-theme");
-      $("body").toggleClass("light-theme");
-      theme = $("body").hasClass("light-theme") ? "light" : "dark";
+      body.removeClass("dark-theme").toggleClass("light-theme");
+      theme = body.hasClass("light-theme") ? "light" : "dark";
     } else {
-      $("body").toggleClass("light-theme");
+      body.toggleClass("light-theme");
       theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
     }
 
